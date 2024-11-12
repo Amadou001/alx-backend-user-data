016d525b41b952a63c10dc680c7dff2ef1962def
Amadou001 <bamadou634@gmail.com>
2024-11-12 15:20:20 -0500

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,17 @@ class BasicAuth(Auth):
         if authorization_header_split[0] != 'Basic':
             return None
         return authorization_header_split[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """returns the decoded value
+        of a Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except Exception as e:
+            return None
